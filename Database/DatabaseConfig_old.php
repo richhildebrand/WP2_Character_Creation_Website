5e24b8eb74071b32a58e80944e1eb98e3879f362
Cody <ckerstin@kent.edu>
2013-05-01 17:12:05 -0400

    Changed database to run from my WP2 database
    Needed so that the project could run from my house.
    webdev.cs.kent.edu/phpmyadmin
    username=ckerstin
    password=810414477

---
+++ Database/DatabaseConfig_old.php
@@ -0,0 +1,4 @@
+<?php
+    define( "DB_DSN","mysql:host=43bae7c2-839d-41eb-bb0a-a19e000dae9f.mysql.sequelizer.com;dbname=db43bae7c2839d41ebbb0aa19e000dae9f"); //this constant will be use as our connectionstring/dsn
+    define( "DB_PASSWORD", "xUJ6gHtosSUgoPC6agAbDNfrmXQpaivg4XxLEGq7q66tiTz38VXTwF2RNrTrZdnj" );
+    define( "DB_USERNAME", "npegbwvrplsuvvoj" ); 
